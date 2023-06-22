9fb3cfcb3df02b311a059723721fa7934fa7ea90
Semih Irmak <smhirmak@hotmail.com>
2023-06-22 11:58:23 +0300

    delete unnecessary codes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
+import Loading from '@/components/ui/Loading';
 import { darkTheme } from '@/theme/theme';
 import createCache from '@emotion/cache';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import { Box, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useTheme } from '@mui/material/styles';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 import { Router } from 'next/router';
 import { useState } from 'react';
 import Layout from '../components/ui/Layout';
 import '../main.css';
-import Loading from '@/components/ui/Loading';
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -25,8 +23,6 @@ export default function App(props: MyAppProps) {
   const { Component, pageProps } = props;
 
   const [loading, setLoading] = useState(false);
-  const themePage = useTheme();
-  const isMobile = useMediaQuery(themePage.breakpoints.down('md'));
 
   Router.events.on('routeChangeStart', (url) => {
     setLoading(true);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   corePlugins: {
     preflight: false
   },
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   important: '#__next',
   theme: {
     extend: {}
