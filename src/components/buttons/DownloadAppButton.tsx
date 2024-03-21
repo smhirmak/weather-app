f94a485ebc9c75322e77843fc0406accce5e8017
TRDOM\mırmak <mirmak@trabilisim.com>
2024-03-21 16:43:51 +0300

    add test button

--- src/components/buttons/DownloadAppButton.tsx
+++ src/components/buttons/DownloadAppButton.tsx
@@ -27,10 +27,33 @@ const DownloadAppButton = () => {
     });
   };
 
+  let deferredPrompt:any;
+    if(typeof window !== 'undefined') {
+
+      window.addEventListener('beforeinstallprompt', (e) => {
+          deferredPrompt = e;
+      });
+    }
+    if (typeof document !== 'undefined') {
+
+      const installApp = document.getElementById('installApp');
+      installApp.addEventListener('click', async () => {
+          if (deferredPrompt !== null) {
+              deferredPrompt.prompt();
+              const { outcome } = await deferredPrompt.userChoice;
+              if (outcome === 'accepted') {
+                  deferredPrompt = null;
+              }
+          }
+      });
+    }
+
   return (
-    <Button variant="outlined" onClick={handleInstallClick}>
-      Install App
-    </Button>
+    // <Button variant="outlined" onClick={handleInstallClick}>
+    //   Install App
+    // </Button>
+    <button id="installApp">Install</button>
+
   );
 };
 
