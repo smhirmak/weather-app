ea784666f5f61e1abb1c37c4bab7436753f44006
TRDOM\mırmak <mirmak@trabilisim.com>
2024-03-21 16:08:58 +0300

    add vercel analystics

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -2,12 +2,14 @@ import { Container } from '@mui/material';
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <Container>
       <Header />
       {children}
+      <Analytics />
       <Footer />
     </Container>
   );
