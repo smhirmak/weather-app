8a000216387bf39986c33cc94b2247a6229b2b15
TRDOM\mırmak <mirmak@trabilisim.com>
2024-03-21 16:31:14 +0300

    fix typescript bug

--- src/components/buttons/DownloadAppButton.tsx
+++ src/components/buttons/DownloadAppButton.tsx
@@ -1,34 +1,37 @@
 import { Button } from '@mui/material';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const DownloadAppButton = () => {
-    const [installPromptEvent, setInstallPromptEvent] = useState<any>(null);
+  const [installPromptEvent, setInstallPromptEvent] = useState<Event | null>(null);
 
-    useEffect(() => {
-      window.addEventListener('beforeinstallprompt', (e: any) => {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        setInstallPromptEvent(e);
-      });
-    }, []);
-  
-    const handleInstallClick = () => {
-      if (!installPromptEvent) return;
-      // Show the prompt
-      installPromptEvent.prompt();
-      // Wait for the user to respond to the prompt
-      installPromptEvent.userChoice.then((choiceResult: any) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-      });
-    };
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e: Event) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setInstallPromptEvent(e);
+    });
+  }, []);
+
+  const handleInstallClick = () => {
+    if (!installPromptEvent) return;
+    // Show the prompt
+    (installPromptEvent as any).prompt();
+    // Wait for the user to respond to the prompt
+    (installPromptEvent as any).userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  };
+
   return (
-    <Button variant="outlined" onClick={handleInstallClick}>Install App</Button>
-  )
-}
+    <Button variant="outlined" onClick={handleInstallClick}>
+      Install App
+    </Button>
+  );
+};
 
-export default DownloadAppButton
+export default DownloadAppButton;
